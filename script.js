30099849e669b18ea488ac58b4940960d5444d91
nimit-pasricha <pasricha.nimit@gmail.com>
2024-06-03 13:20:14 +0400

    Write function to display books on screen

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
 const myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(title, author, numPages, haveRead) {
+  this.title = title;
+  this.author = author;
+  this.numPages = numPages;
+  this.haveRead = haveRead;
 }
 
-function addBookToLibrary() {
-  // do stuff here
+function addBookToLibrary() {}
+addBookToLibrary();
+
+function displayBooks() {
+  for (let oneBook of myLibrary) {
+    const book = document.createElement("div");
+    book.className = "book";
+    document.querySelector(".display-area").appendChild(book);
+
+    const bookTitle = book.appendChild(document.createElement("div"));
+    bookTitle.textContent = oneBook.title;
+
+    const bookAuthor = book.appendChild(document.createElement("div"));
+    bookAuthor.textContent = oneBook.author;
+
+    const bookNumPages = book.appendChild(document.createElement("div"));
+    bookNumPages.textContent = oneBook.numPages;
+
+    const haveReadBook = book.appendChild(document.createElement("div"));
+    haveReadBook.textContent = oneBook.haveRead
+      ? "Finished Reading"
+      : "Not read yet";
+  }
 }
+
+displayBooks();
